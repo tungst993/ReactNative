3485dab9f88d1b7a03d76c099ec610cfb59b0727
tungst993 <vantung1412@gmail.com>
2019-05-22 16:29:14 +0700

    Add code

--- Calculator/App.js
+++ Calculator/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 // const instructions = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -7,12 +7,104 @@ import { StyleSheet, Text, View } from 'react-native';
 // });
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      resultText: '',
+      result: ''
+    }
+    this.ops = ['D', '+', '-', '*', '/']
+
+  }
+
+  buttonPressed = (text) => {
+
+    if (text == '=') {
+      return this.calculateResult()
+    }
+
+    this.setState({
+      resultText: `${this.state.resultText}${text}`
+    })
+  }
+
+  calculateResult = () => {
+    const lastChar = this.state.resultText.split('').pop()
+
+    if(this.ops.indexOf(lastChar) > 0) return
+    this.setState({
+      result: eval(this.state.resultText),
+    })
+  }
+
+  operate = (operation) => {
+    const { resultText } = this.state
+    switch (operation) {
+      case 'D':
+        let text = resultText.slice(0, resultText.length - 1)
+        this.setState({
+          resultText: text
+        })
+        break
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      const lastChar = resultText.split('').pop()
+
+      if(this.ops.indexOf(lastChar) > 0) return
+      if(this.state.text == '') return 
+      this.setState({
+        resultText: resultText + operation
+      })
+    }
+  }
+
   render() {
+    console.log(this.state.resultText)
+    const {
+      resultText,
+      result
+    } = this.state
+    let rows = []
+    let number = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['.', '0', '=']]
+    for (let i = 0; i < 4; i++) {
+      let row = []
+      for (let k = 0; k < 3; k++) {
+        row.push(
+          <TouchableOpacity style={styles.btn} key={k} onPress={() => this.buttonPressed(number[i][k])}>
+            <Text style={styles.btnText}>{number[i][k]}</Text>
+          </TouchableOpacity>
+        )
+      }
+      rows.push(<View key={i} style={styles.row}>{row}</View>)
+    }
+
+    let operations = []
+    for (let i = 0; i < 5; i++) {
+      operations.push(
+        <TouchableOpacity style={styles.btn} key={i} onPress={() => this.operate(this.ops[i])}>
+          <Text style={[styles.btnText, styles.white]}>{this.ops[i]}</Text>
+        </TouchableOpacity>
+      )
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>{resultText}</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>{result}</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>
+            {rows}
+          </View>
+          <View style={styles.operations}>
+            {operations}
+          </View>
+        </View>
       </View>
     );
   }
@@ -21,18 +113,59 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  result: {
+    flex: 2,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  btn: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignSelf: 'stretch'
   },
-  welcome: {
+  white: {
+    color: 'white'
+  },
+  btnText: {
+    fontSize: 30
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexGrow: 3
+  },
+  numbers: {
+    flex: 7,
+    backgroundColor: 'yellow'
+  },
+  operations: {
+    flex: 2,
+    backgroundColor: 'black',
+    justifyContent: 'space-around'
+  },
+  row: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  button: {
+    flex: 1,
+  },
+  resultText: {
+    fontSize: 30,
+    color: 'white'
+  },
+  calculationText: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    color: 'white'
+  }
 });
